2e9cd297f80d92441b3ebe5591a594ed44f69d95
Franco Canzani <francocanzani@gmail.com>
2023-10-21 12:56:03 +0200

    Server started

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+
+app.get('/:id', (req, res) => {
+  res.json({
+    message: 'URL shortener tooling',
+  });
+});
+
+const port = process.env.Port || 3001;
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
