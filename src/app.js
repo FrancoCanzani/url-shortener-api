52ab405d5d2662cb1c4171fb9adb63f34438d615
Franco Canzani <francocanzani@gmail.com>
2023-10-28 13:20:14 +0200

    Extra config on rate limiter

--- src/app.js
+++ src/app.js
@@ -23,16 +23,13 @@ app.use(express.json());
 // Create a rate limiter with desired settings
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Max requests per windowMs
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
 
 // Apply the rate limiter to specific routes or all routes
 app.use('/api', limiter);
-
-// Custom error handler for rate limiting exceeded
-app.use((req, res) => {
-  res.status(429).json({ error: 'Rate limit exceeded' });
-});
 
 // global middleware to validate api key
 app.use(validateApiKey);
